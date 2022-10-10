6e3cb32ff8a397f8787653d93dfce0634234fec0
LinneaJohansen <linnea_johansen@outlook.com>
2022-10-10 09:46:53 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,4 @@ INSERT INTO student (name, picture, status, bio, fun_fact) VALUES ('Jack', 'http
                                                                    'Online', 'I lived at a hotel once', 'Totally not crazy');
 INSERT INTO student (name, picture, status, bio, fun_fact) VALUES ('Tony', 'https://upload.wikimedia.org/wikipedia/en/1/19/Tony_Montana_in_Scarface_%281983%29%2C_portrayed_by_Al_Pacino.jpg',
                                                                    'Offline', 'Kingpin', 'I get really angry when people do not greet my little friend');
+
