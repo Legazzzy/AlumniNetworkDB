feefa779b179ada11c799c615bc090aa7cf1bf57
Legazzzy <marius.eriksen13@yahoo.no>
2022-10-11 10:10:56 +0200

    heroku deployment

--- src/main/java/com/example/alumninetworkcase/AlumniNetworkCaseApplication.java
+++ src/main/java/com/example/alumninetworkcase/AlumniNetworkCaseApplication.java
@@ -1,13 +1,21 @@
 package com.example.alumninetworkcase;
 
+import com.example.alumninetworkcase.utils.MyConfigs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class AlumniNetworkCaseApplication {
 
 	public static void main(String[] args) {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+		ctx.register(MyConfigs.class);
+		ctx.refresh();
+		ctx.close();
+
 		SpringApplication.run(AlumniNetworkCaseApplication.class, args);
+
 	}
 
 }
